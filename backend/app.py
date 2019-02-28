b1d2cfe8a391f537e33ed2ea03371800550769f0
Wing <chanwingjun3@gmail.com>
2019-02-27 20:18:00 -0500

    Stored information on MongoDB successfully. Awaiting render_template page from front end to use

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 app = Flask(__name__, template_folder="../frontend/build", static_folder="../frontend/build/static") # remember to compile React app separately
 
@@ -21,29 +22,37 @@ def main():
 
 # login
 
+client = MongoClient('localhost', 27017)
+db = client['mydb']
+collection = db['users']
+
+
 # sign up
-
-
-@app.route('/register', methods=['POST'])
-def register():
+@app.route('/signup', methods=['POST'])
+def signup():
     if(request.method == 'POST'):
+        if(request.form['submit'] == 'Enter'):
         # store account information into database (create new user)
         # user email
         # user login
         # user password
         #error checking
+            account = {
+                    "first_name": request.form['firstname'],
+                    "last_name": request.form['lastname'],
+                    "username": request.form['username'],
+                    "password": request.form['password'],
+                    "email": request.form['email']
+            }
+            accounts = db.accounts
+            account_id = accounts.insert_one(account).inserted_id
 
-        db.createUser({
-            user: #getuserinfohere
-            pwd: #getuserpasswordhere
-            email: #getuseremailhere
+    else:
+        pass
 
-        })
-
-
-    else:   #request.method == 'GET'
-        return render_template('register.html')
-
+    #Wait for the render_template from front end page
+    return render_template('index.html', title="Sign Up")
+            
 
 
 
