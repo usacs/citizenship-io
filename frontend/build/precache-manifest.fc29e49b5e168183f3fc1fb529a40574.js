aea55d49d185d04b1920cf2cf0bf104a2619234b
Amulya <amulya.mummaneni@gmail.com>
2019-03-11 17:15:02 -0400

    node_modules problem

---
+++ frontend/build/precache-manifest.fc29e49b5e168183f3fc1fb529a40574.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0088089c5828c7df30b4",
+    "url": "/static/js/main.0088089c.chunk.js"
+  },
+  {
+    "revision": "53f3cac46a7e8a0c3587",
+    "url": "/static/js/1.53f3cac4.chunk.js"
+  },
+  {
+    "revision": "0088089c5828c7df30b4",
+    "url": "/static/css/main.f823585a.chunk.css"
+  },
+  {
+    "revision": "2c83147a214241ef74c373c7f1e023e3",
+    "url": "/index.html"
+  }
+];
