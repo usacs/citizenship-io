3ba13b751f07df7b54343478a6803c7c44e0797e
ReedKrawiec <reedkrawiec@gmail.com>
2019-12-30 16:54:44 -0500

    Created public branch

---
+++ build/precache-manifest.126948a9e907b45bcad1e197be6e7dd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e1ee7b1573e0877ea2c",
+    "url": "/build/static/css/main.c5bf1f64.chunk.css"
+  },
+  {
+    "revision": "8e1ee7b1573e0877ea2c",
+    "url": "/build/static/js/main.8e1ee7b1.chunk.js"
+  },
+  {
+    "revision": "3d6782bac4ceded81c64",
+    "url": "/build/static/js/1.3d6782ba.chunk.js"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "f41fcc1285bce3a1b3b85b8f967f94fb",
+    "url": "/build/index.html"
+  }
+];
